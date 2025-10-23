3ea39a0ecd3f1a74256dc0ed789bd149aa9e7162
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2025-10-23 16:01:16 +0530

    Leetcode 48 in cpp
    Implemented two methods to rotate a matrix 90 degrees clockwise.

---
+++ leetcode48/solution.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int edgeLength = matrix.size();
+
+        int top = 0;
+        int bottom = edgeLength - 1;
+
+        while (top < bottom) {
+            for (int col = 0; col < edgeLength; col++) {
+                int temp = matrix[top][col];
+                matrix[top][col] = matrix[bottom][col];
+                matrix[bottom][col] = temp;
+            }
+            top++;
+            bottom--;
+        }
+
+        for (int row = 0; row < edgeLength; row++) {
+            for (int col = row + 1; col < edgeLength; col++) {
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[col][row];
+                matrix[col][row] = temp;
+            }
+        }        
+    }
+};
+
+// another solution
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0; i<(n+1)/2; i++) {
+            for(int j=0; j<(n/2); j++) {
+                int temp = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+};
