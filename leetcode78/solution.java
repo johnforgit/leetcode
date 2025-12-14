d039afee20f5f3b515120de23893be36bab5d33c
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2025-12-14 19:53:34 +0530

    Leetcode 78 subsets solution in Java

---
+++ leetcode78/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void dfs(int[] nums, int start, List<Integer> sub, List<List<Integer>> res) {
+        res.add(new ArrayList<>(sub));
+
+        for (int i = start; i < nums.length; i++) {
+            sub.add(nums[i]);
+            dfs(nums, i + 1, sub, res);
+            sub.remove(sub.size() - 1); // backtrack
+        }
+    }
+}
