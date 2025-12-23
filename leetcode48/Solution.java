f1bc6cc15c24b1fbaf20aff0e036f3b68b3c731a
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2025-12-23 22:52:47 +0530

    Refactor matrix rotation implementation
    Contains few more solutions to solve matrix rotation leetcode problem

--- leetcode48/Solution.java
+++ leetcode48/Solution.java
@@ -1,33 +1,61 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        
-        // first transform the matrix to its transpose
         int rows=matrix.length;
-        int cols=matrix.length;
-        for(int i = 0;i < rows;i++) {
-            for(int j = i+1;j < cols;j++) {
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
+        int cols=matrix[0].length;
+        
+        // transpose the matrix
+        for(int i=0; i<rows; i++) {
+            for(int j=i+1; j<cols; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
                 matrix[j][i]=temp;
             }
         }
 
-        // now reverse each row in the matrix
-        for(int i = 0;i < rows;i++) {
-            int left=0;
-            int right=rows-1;
-            while(left<right) {
-                int temp=matrix[i][left];
-                matrix[i][left]=matrix[i][right];
-                matrix[i][right]=temp;
-                left++;
-                right--;
+        // reverse each rows in the matrix
+        for(int i=0; i<rows; i++) {
+            int L=0, R=rows-1;
+            while(L<R) {
+                int temp=matrix[i][L];
+                matrix[i][L]=matrix[i][R];
+                matrix[i][R]=temp;
+                L++; R--;
             }
         }
-    }
-}
 
-public void rotate(int[][] mat) {
+        /**
+        another way to reverse the matrix
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n/2; j++) {
+                int temp = matrix[i][j]
+                matrix[i][j] = matrix[i][n-j-1]
+                matrix[i][n-j-1] = matrix[i][j]
+            }
+        }
+         */
+    }
+
+  public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        // flip the matrix top to bottom
+        for(int i=0, j=n-1; i<j; j--,i++) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[j];
+            matrix[j] = temp;
+        }
+
+        // find the transpose of the matrix
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+    }
+
+    public void rotate(int[][] mat) {
         int n = mat.length;
         for(int i = 0;i < (n+1)/2;i++) {
             for(int j = 0;j < n/2;j++) {
@@ -39,3 +67,4 @@ public void rotate(int[][] mat) {
             }
         }
     }
+}
