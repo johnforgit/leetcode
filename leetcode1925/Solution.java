4b334099b21b1afabe06ae26c0d2905f68130211
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2025-12-23 22:56:24 +0530

    Leetcode 1925 in Java

---
+++ leetcode1925/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countTriples(int n) {
+        int tripletCount=0;
+        // enumerate a and b
+        for(int a=1; a<=n; ++a) {
+            for(int b=1; b<=n ; ++b) {
+                int c = (int)Math.sqrt(a*a + b*b + 1.0);
+                if(c<=n && (c*c == a*a + b*b)) {
+                    ++tripletCount;
+                }
+            }
+        }
+        return tripletCount;
+    }
+}
