f1c745b95906e019910be56abf2e5acaf25cf3f3
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2025-12-26 00:51:14 +0530

    Added another solution to the leetcode qn

--- Leetcode200. Number of islands/Solution.py
+++ Leetcode200. Number of islands/Solution.py
@@ -1,5 +1,6 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        '''
         rows=len(grid)
         cols=len(grid[0])
         num_islands=0
@@ -20,5 +21,28 @@ class Solution:
                     grid[row][col] = '2' # mark the existing cell
                     dfs(row, col) # perform dfs from that cell position
         
+        return num_islands
+        '''
+
+        rows, cols = len(grid), len(grid[0])
+        num_islands=0
+
+        def dfs(i, j):
+            if(i<0 or i>=rows or j<0 or j>=cols or grid[i][j]!='1'):
+                return
+            grid[i][j]='0'
+            # explore in all the 4 directions
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1':
+                    num_islands += 1
+                    dfs(i, j)
+
         return num_islands
         
