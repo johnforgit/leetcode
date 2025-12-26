199b4c560b8e8925a66a7c510c06341de74c544f
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2025-12-26 23:20:36 +0530

    Leetcode 695 in C++

---
+++ Leetcode695. Max Area of Island/Solution.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int rows=grid.size();
+        int cols = grid[0].size();
+        int max_area = 0;
+
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                if(grid[i][j]==1) { // if the cell is land
+                    int area = dfs(grid, i, j);
+                    max_area = max(max_area, area);
+                }
+            }
+        }
+        return max_area;
+    }
+
+    int dfs(vector<vector<int>>& grid, int i, int j) {
+        // boundary and water check
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0)
+            return 0;
+
+        // mark as visited
+        grid[i][j] = 0;
+
+        int cur_area = 1;
+        // explore all 4 directions
+        cur_area += dfs(grid, i + 1, j);
+        cur_area += dfs(grid, i - 1, j);
+        cur_area += dfs(grid, i, j + 1);
+        cur_area += dfs(grid, i, j - 1);
+
+        return cur_area;
+    }
+};
+
+// runtime - 0 ms
+
+class Solution {
+public:
+
+    int dfs(vector<vector<int>>& grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.size() || j >=grid[0].size() || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0;
+        return (1 + dfs(grid, i + 1, j) + dfs(grid, i, j + 1) + dfs(grid, i - 1, j) + dfs(grid, i, j - 1));
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int maxArea = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                maxArea = max(maxArea, dfs(grid, i, j));
+            }
+        }
+        return maxArea;
+    }
+};
