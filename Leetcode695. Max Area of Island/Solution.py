6f6dd53a96321ff46ad8fac22a7ad0c6c9d6f802
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2025-12-26 23:30:02 +0530

    Greg Hogg's solution to the problem

--- Leetcode695. Max Area of Island/Solution.py
+++ Leetcode695. Max Area of Island/Solution.py
@@ -24,3 +24,24 @@ class Solution:
                     max_area=max(max_area, area)
         
         return max_area
+
+# solution by greg hogg
+class Solution:
+    def maxAreaOfIsland(self, grid:List[List[int]]) -> int:
+        m=leng(grid)
+        n=len(grid)
+
+        def dfs(i, j):
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j]==0:
+                return 0
+            else:
+                grid[i][j]=0
+                return 1+dfs(i+1, j)+dfs(i-1, j)+dfs(i,j-1)+dfs(i,j+1)
+
+        max_area=0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    cur_area = dfs(i,j)
+                    max_area = max(max_area, cur_area)
+        return max_area
