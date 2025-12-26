b570bde4b717e5c0195bff28dd717f0eb29d178f
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2025-12-26 23:43:46 +0530

    Added neetcode io's solution

--- Leetcode463. Island Perimeter/Solution.py
+++ Leetcode463. Island Perimeter/Solution.py
@@ -34,6 +34,26 @@ class Solution:
 
         return perimeter
 
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        result = 0
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    result += 4
+                    
+                    if r > 0 and grid[r-1][c] == 1:
+                        result -= 2
+                        
+                    if c > 0 and grid[r][c-1] == 1:
+                        result -= 2
+        
+        return result
+
 # runtime - 17ms
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
@@ -85,3 +105,26 @@ class Solution:
         return ans
 
 
+# neetcode solution
+class Solution:
+    def islandPerimeter(class, grid:List[List[int]]) -> int:
+        visit = set()
+
+        def dfs(i, j):
+            if i>=len(grid) or j>=len(grid[0]) or i<0 or j<0 or grid[i][j] == 0:
+                return 1
+            if(i, j) in visit:
+                return 0
+
+            # add the row and column to the visited set
+            visit.add((i,j))
+            perim = dfs(i, j+1)
+            perim += dfs(i, j-1)
+            perim += dfs(i+1, j)
+            perim += dfs(i-1, j)
+            return perim
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0]):
+                if grid[i][j]:
+                    return dfs(i, j);
