99008bd7bb6acc3f0a3704412818386d9c580f16
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2025-12-26 23:55:34 +0530

    Added another solution in Python

--- Leetcode2483. Min penalty for a shop/Solution.py
+++ Leetcode2483. Min penalty for a shop/Solution.py
@@ -12,3 +12,22 @@ class Solution:
         return BEST_CLOSING_HOUR
 
 
+# runtime - 0 ms
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        c = customers
+        length = len(c)
+        result = 0
+        resultIdx = -1
+        accum = 0
+        for i in range(length):
+            if c[i] == "Y":
+                accum += 1
+            else:
+                accum -= 1
+            
+            if accum > result:
+                result = accum
+                resultIdx = i
+        
+        return resultIdx + 1
