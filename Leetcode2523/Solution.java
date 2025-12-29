c9e82b1b6c73678bc3f559807cd6c1762d66850a
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2025-12-29 13:08:24 +0530

    Added a solution that has a runtime of 6 ms

--- Leetcode2523/Solution.java
+++ Leetcode2523/Solution.java
@@ -38,3 +38,45 @@ class Solution {
         return prime;
     }
 }
+
+
+// runtime - 6 ms
+class Solution {
+    public int[] closestPrimes(int left, int right) {
+        int diff = Integer.MAX_VALUE;
+        int[] ans = new int[2];
+        ans[0] = -1;
+        ans[1] = -1;
+        int prev = -1;
+        for(int i = left; i <= right; i++) {
+        	if(isPrime(i)) {
+        		if(prev != -1) {
+        			if(diff > i - prev) {
+        				ans[0] = prev;
+        				ans[1] = i;
+                        diff = i - prev;
+                        if(diff <= 2){
+                            return ans;
+                        }
+        			}
+        		}
+        		prev = i;
+        	}
+        }
+        return ans;
+        
+    }
+
+	private boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        if (n <= 3)
+            return true;
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+        for (int i = 5; i * i <= n; i = i + 6)
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        return true;
+	}
+}
