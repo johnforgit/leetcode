674bc36746a61f1f78c6bc2171caa5627771aa66
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2025-12-31 22:39:54 +0530

    Leetcode 474 in java

---
+++ Leetcode474. Ones and Zeroes/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        
+        for(String s:strs) {
+            int zeros=0,ones=0;
+            for(char c:s.toCharArray()) {
+                if(c=='0')
+                    zeros++;
+                else 
+                    ones++;
+            }
+
+            for(int z=m; z>=zeros; z--) {
+                for(int o=n; o>=ones; o--) {
+                    dp[z][o] = Math.max(dp[z][o],
+                            dp[z-zeros][o-ones]+1);
+                }
+            }
+        }
+
+        
+
+        return dp[m][n];
+    }
+}
