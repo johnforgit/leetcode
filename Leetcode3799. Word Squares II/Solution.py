2a54f1e90f8ca4d0bb189909941fc9ac1a29cf01
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-01-04 23:28:09 +0530

    Added a solution that runs in 18 ms

--- Leetcode3799. Word Squares II/Solution.py
+++ Leetcode3799. Word Squares II/Solution.py
@@ -9,4 +9,33 @@ class Solution:
         res.sort()
         return res
 
-  
+
+# runtime - 18 ms
+class Solution:
+    def wordSquares(self, words: List[str]) -> List[List[str]]:
+        res=[]
+        n=len(words)
+        start_char = defaultdict(list) # keep a list of words and their starting letter
+        words.sort()
+        for i in words:
+            start_char[i[0]].append(i)
+        
+        for i in range(n):
+            t=words[i] # take a word from words
+            # check for words in the dictionary with the same first letter
+            for j in start_char[t[0]]: 
+                if j==t: # if both words are equal continue
+                    continue
+                for k in start_char[t[3]]: # check for words with the same 4th letter
+                    if j==k or t==k: # if they're the same continue
+                        continue
+                    left_end = j[3]
+                    right_end = k[3]
+                    for b in start_char[left_end]:
+                        if(b[3] == right_end):
+                            if b not in [t,j,k]:
+                                res.append((t,j,k,b))
+
+        
+        return sorted(res)
+
