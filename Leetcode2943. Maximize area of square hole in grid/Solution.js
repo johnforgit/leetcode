2a7b84abe6a7be01538e5e0089c86d397b1bb125
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-01-15 19:17:39 +0530

    Leetcode 2943 in js

---
+++ Leetcode2943. Maximize area of square hole in grid/Solution.js
@@ -0,0 +1,26 @@
+function maximizeSquareHoleArea(n, m, hBars, vBars) {
+    hBars.sort((a, b) => a - b);
+    vBars.sort((a, b) => a - b);
+    let hmax = 1,
+        vmax = 1;
+    let hcur = 1,
+        vcur = 1;
+    for (let i = 1; i < hBars.length; i++) {
+        if (hBars[i] === hBars[i - 1] + 1) {
+            hcur++;
+        } else {
+            hcur = 1;
+        }
+        hmax = Math.max(hmax, hcur);
+    }
+    for (let i = 1; i < vBars.length; i++) {
+        if (vBars[i] === vBars[i - 1] + 1) {
+            vcur++;
+        } else {
+            vcur = 1;
+        }
+        vmax = Math.max(vmax, vcur);
+    }
+    const side = Math.min(hmax, vmax) + 1;
+    return side * side;
+}
