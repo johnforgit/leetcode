34e8142c38f4981a55211b793a1c12e50be78c8f
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-01-18 21:21:53 +0530

    Added solution that runs in 2 ms

--- Leetcode1261. Find elements in a contaminated binary tree/Solution.cpp
+++ Leetcode1261. Find elements in a contaminated binary tree/Solution.cpp
@@ -239,3 +239,52 @@ private:
  * FindElements* obj = new FindElements(root);
  * bool param_1 = obj->find(target);
  */
+
+
+// runtime - 2ms
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+public:
+    unordered_set<int>seen;
+    FindElements(TreeNode* root) {
+        bfs(root);
+    }
+    
+    bool find(int target) {
+     return seen.find(target)!=seen.end();   
+    }
+    void bfs(TreeNode* root){
+        queue<TreeNode*>q;
+        root->val=0;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* front=q.front();
+            q.pop();
+            seen.insert(front->val);
+            if(front->right){
+                front->right->val=(2*front->val +2);
+                q.push(front->right);
+            }
+             if(front->left){
+                front->left->val=(2*front->val +1);
+                q.push(front->left);
+            }
+        }
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
