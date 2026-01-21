2127e522cfba1f5781a92efad9f739f4bf8c1fd0
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-01-21 23:26:46 +0530

    Leetcode 3315 in Python

---
+++ Leetcode3315. Construct min bitwise array II/Solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        res = []
+        for num in nums:
+            if num == 2:
+                res.append(-1)
+            else:
+                lowest_set_bit = (num+1) & (-(num+1))
+                min_value = num - (lowest_set_bit//2)
+                res.append(min_value)
+
+        return res
+
+# runtime - 0ms
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            res = -1
+            d = 1
+            while (nums[i] & d) != 0:
+                res = nums[i] - d
+                d <<= 1
+            nums[i] = res
+        return nums
