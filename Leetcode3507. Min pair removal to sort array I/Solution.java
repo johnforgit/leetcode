51464ac5da6e46261b3f6a9ac7f94f3c6a10102d
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-01-22 22:57:30 +0530

    Leetocde 3057 in java

---
+++ Leetcode3507. Min pair removal to sort array I/Solution.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class Solution {
+    public int minimumPairRemoval(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int x : nums) list.add(x);
+
+        int count = 0;
+
+        while (list.size() > 1) {
+            boolean isAscending = true;
+            int minSum = Integer.MAX_VALUE;
+            int targetIndex = -1;
+
+            for (int i = 0; i < list.size() - 1; i++) {
+                int pairSum = list.get(i) + list.get(i + 1);
+
+                if (list.get(i) > list.get(i + 1)) {
+                    isAscending = false;
+                }
+
+                if (pairSum < minSum) {
+                    minSum = pairSum;
+                    targetIndex = i;
+                }
+            }
+
+            if (isAscending) break;
+
+            count++;
+            list.set(targetIndex, minSum);
+            list.remove(targetIndex + 1);
+        }
+
+        return count;
+    }
+}
+
+
+// runtime - 1ms
+class Solution {
+    public int minimumPairRemoval(final int[] nums) {
+        int n = nums.length, count = 0;
+
+        while(n > 1) {
+            int minSum = Integer.MAX_VALUE, minIdx = -1;
+            boolean decreasing = true;
+
+            for(int i = 1; i < n; ++i) {
+                if(nums[i - 1] + nums[i] < minSum) {
+                    minSum = nums[i - 1] + nums[i];
+                    minIdx = i - 1;
+                }
+
+                if(nums[i - 1] > nums[i])
+                    decreasing = false;
+            }
+
+            if(decreasing)
+                return count;
+
+            nums[minIdx] = minSum;
+
+            for(int i = minIdx + 1; i < n - 1; ++i)
+                nums[i] = nums[i + 1];
+
+            count++;
+            n--;
+        }
+
+        return count;
+    }
+}
