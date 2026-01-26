00f6f71cea1e99a184c3103de7aeed384a480ef1
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-01-27 00:17:09 +0530

    Update Solution.java

--- Leetcode1351. Count Negative nos in a sorted matrix/Solution.java
+++ Leetcode1351. Count Negative nos in a sorted matrix/Solution.java
@@ -1,5 +1,20 @@
 class Solution {
     public int countNegatives(int[][] grid) {
+        int index = grid[0].length-1;
+        int res = 0;
+
+        for(int[] row:grid) {
+            while(index>=0 && row[index]<0) {
+                index--;
+            }
+            res += (grid[0].length-1) - index;
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public int countNegatives(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
         int i=rows-1;
