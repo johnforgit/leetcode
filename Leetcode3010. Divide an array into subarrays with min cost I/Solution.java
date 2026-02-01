26d42a6256cd189423e8374ec9f3d6f90a53976f
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-02-01 23:52:30 +0530

    Leetcode 3010 in Java

---
+++ Leetcode3010. Divide an array into subarrays with min cost I/Solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minimumCost(int[] nums) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } else if (nums[i] < min2) {
+                min2 = nums[i];
+            }
+        }
+        
+        return nums[0] + min1 + min2;
+    }
+}
+
+
+# runtime - 0ms
+  class Solution {
+    public int minimumCost(int[] nums) {
+         int first = nums[0];
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } else if (nums[i] < min2) {
+                min2 = nums[i];
+            }
+        }
+
+        return first + min1 + min2;
+    }
+}
