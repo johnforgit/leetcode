7f60a4f8af5c806601b1c46dc36933c318f36d8f
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-02-03 23:19:42 +0530

    Leetcode 3637 in Java

---
+++ Leetcode3637. Trionic array I/Solution.java
@@ -0,0 +1,63 @@
+class Solution {
+
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        if (nums[0] >= nums[1]) {
+            return false;
+        }
+        int count = 1;
+        for (int i = 2; i < n; i++) {
+            if (nums[i - 1] == nums[i]) {
+                return false;
+            }
+            if ((nums[i - 2] - nums[i - 1]) * (nums[i - 1] - nums[i]) < 0) {
+                count++;
+            }
+        }
+        return count == 3;
+    }
+}
+
+
+// runtime - 0ms
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        
+        // Find p: first position where array stops increasing
+        int p = 0;
+        while (p < n - 1 && nums[p] < nums[p + 1]) {
+            p++;
+        }
+        
+        // p must be > 0 (there must be an increasing section)
+        if (p == 0) {
+            return false;
+        }
+        
+        // Find q: first position after p where array stops decreasing
+        int q = p;
+        while (q < n - 1 && nums[q] > nums[q + 1]) {
+            q++;
+        }
+        
+        // q must be > p (there must be a decreasing section)
+        if (q == p) {
+            return false;
+        }
+        
+        // q must be < n - 1 (there must be an increasing section after q)
+        if (q == n - 1) {
+            return false;
+        }
+        
+        // Check that the rest is strictly increasing
+        for (int i = q; i < n - 1; i++) {
+            if (nums[i] >= nums[i + 1]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
