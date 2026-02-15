a98707c94b5de5bbfdc3fdb13600bb3805b0fb3b
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-02-15 23:33:25 +0530

    Leetcode 67 in Python
    Implement addBinary method using half adder logic.

---
+++ Leetcode67. Add Binary/Solution.py
@@ -0,0 +1,11 @@
+Using Half Adder
+
+class Solution:
+    def addBinary(self, a, b) -> str:
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            x, y = x ^ y, (x & y) << 1
+        return bin(x)[2:]
+
+
+Using Full Adder
