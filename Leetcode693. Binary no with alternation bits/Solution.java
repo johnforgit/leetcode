172e36c9a1df9ece9a3204fe67cbe56afb23ec1e
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-02-19 13:01:48 +0530

    Leetcode 693 in Java

---
+++ Leetcode693. Binary no with alternation bits/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        String bits = Integer.toBinaryString(n);
+        for(int i=0; i<bits.length()-1; i++) {
+            if(bits.charAt(i) == bits.charAt(i+1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int cur = n % 2;
+        n /= 2;
+        while (n > 0) {
+            if (cur == n % 2) return false;
+            cur = n % 2;
+            n /= 2;
+        }
+        return true;
+    }
+}
