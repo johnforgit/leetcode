928e0fc7cecd8b1592c915b065201b40dfaf303a
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-02-23 23:49:32 +0530

    Update solution.java

--- Leetcode1461. Check if a string contains all binary codes of size k/Solution.java
+++ Leetcode1461. Check if a string contains all binary codes of size k/Solution.java
@@ -1,17 +1,20 @@
-class Solution:
-    def hasAllCodes(self, s: str, k: int) -> bool:
-        req = 1<<k
-        seen = [False] * req
-        mask = req-1
-        h = 0
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        int req = 1 << k;
+        boolean[] seen = new boolean[req];
+        int mask = req - 1;
+        int hash = 0;
 
-        for i, ch in enumerate(s):
-            h = ((h<<1) & mask) | (ord(ch)&1)
+        for (int i = 0; i < s.length(); ++i) {
+            hash = ((hash << 1) & mask) | (s.charAt(i) & 1);
 
-            if (i >= k-1) and not seen[h]:
-                seen[h] = True
-                req -= 1
-                if req ==0:
-                    return True
+            if (i >= k - 1 && !seen[hash]) {
+                seen[hash] = true;
+                req--;
+                if (req == 0) return true;
+            }
+        }
 
-        return False
+        return false;
+    }
+}
