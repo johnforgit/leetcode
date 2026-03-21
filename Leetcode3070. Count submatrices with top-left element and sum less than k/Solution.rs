f8348519479b2769ac47aa08584e6cc34ae39d32
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-03-21 19:39:21 +0530

    Create Solution.rs

---
+++ Leetcode3070. Count submatrices with top-left element and sum less than k/Solution.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn count_submatrices(grid: Vec<Vec<i32>>, k: i32) -> i32 {
+        let n = grid.len();
+        let m = grid[0].len();
+        let mut cols = vec![0; m];
+        let mut res = 0;
+
+        for i in 0..n {
+            let mut row_sum = 0;
+            for j in 0..m {
+                cols[j] += grid[i][j];
+                row_sum += cols[j];
+                if row_sum <= k {
+                    res += 1;
+                }
+            }
+        }
+
+        res
+    }
+}
