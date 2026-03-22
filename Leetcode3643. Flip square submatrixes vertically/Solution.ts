e70f7b0e70f4347047e2105dbe78650f8c24948a
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-03-22 18:09:45 +0530

    Update Solution.ts

--- Leetcode3643. Flip square submatrixes vertically/Solution.ts
+++ Leetcode3643. Flip square submatrixes vertically/Solution.ts
@@ -11,3 +11,28 @@ function reverseSubmatrix(
     }
     return grid;
 }
+
+
+// runtime - 0ms
+
+
+function reverseSubmatrix(grid: number[][], x: number, y: number, k: number): number[][] {
+    let row = x;
+    let endRow = x + k - 1;
+    let col = y;
+    let endCol = y + k - 1;
+
+    while (row <= endRow) {
+        while (col <= endCol) {
+            const temp = grid[row][col];
+            grid[row][col] = grid[endRow][col];
+            grid[endRow][col] = temp;
+            col++;
+        }
+        col = y;
+        row++;
+        endRow--;
+    }
+
+    return grid;
+};
