2e3175452dad9de5a8483008c58cf68fa62c7aed
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-03-25 23:27:34 +0530

    Update Solution.go

--- Leetcode2906. Construct Product matrix/Solution.go
+++ Leetcode2906. Construct Product matrix/Solution.go
@@ -24,3 +24,54 @@ func constructProductMatrix(grid [][]int) [][]int {
 
 	return p
 }
+
+
+// runtime - 0ms
+func constructProductMatrix(grid [][]int) [][]int {
+	const MOD = 12345
+	const SHIFT = 32
+	const MASK = 1<<SHIFT - 1
+	prefix := 1
+	for i, row := range grid {
+		for j := range row {
+			val := grid[i][j]
+			grid[i][j] = (prefix << SHIFT) | (val % MOD)
+			prefix = (prefix * (val % MOD)) % MOD
+		}
+	}
+	suffix := 1
+	for i := len(grid) - 1; i >= 0; i-- {
+		for j := len(grid[i]) - 1; j >= 0; j-- {
+			prefix, val := grid[i][j]>>SHIFT, grid[i][j]&MASK
+			grid[i][j] = (prefix * suffix) % MOD
+			suffix = (suffix * val) % MOD
+		}
+	}
+	return grid
+}
+
+
+
+// runtime - 1ms
+func constructProductMatrix(grid [][]int) [][]int {
+	const MOD = 12345
+	const SHIFT = 16
+	const MASK = 1<<SHIFT - 1
+	prefix := 1
+	for i, row := range grid {
+		for j := range row {
+			val := grid[i][j]
+			grid[i][j] = (prefix << SHIFT) | (val % MOD)
+			prefix = (prefix * (val % MOD)) % MOD
+		}
+	}
+	suffix := 1
+	for i := len(grid) - 1; i >= 0; i-- {
+		for j := len(grid[i]) - 1; j >= 0; j-- {
+			prefix, val := grid[i][j]>>SHIFT, grid[i][j]&MASK
+			grid[i][j] = (prefix * suffix) % MOD
+			suffix = (suffix * val) % MOD
+		}
+	}
+	return grid
+}
