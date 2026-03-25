79c6e1e9c4afd82f3100c8eaf78b4d0abb7deba8
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-03-25 23:15:02 +0530

    Create Solution.c

---
+++ Leetcode3546. Equal Sums grid Partition I/Solution.c
@@ -0,0 +1,28 @@
+bool canPartitionGrid(int** grid, int gridSize, int* gridColSize) {
+    int m = gridSize, n = gridColSize[0];
+    long long total = 0;
+    
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            total += grid[i][j];
+    
+    if (total % 2) return false;
+    
+    long long target = total / 2, sum = 0;
+    
+    for (int i = 0; i < m - 1; i++) {
+        for (int j = 0; j < n; j++)
+            sum += grid[i][j];
+        if (sum == target) return true;
+    }
+    
+    sum = 0;
+    
+    for (int j = 0; j < n - 1; j++) {
+        for (int i = 0; i < m; i++)
+            sum += grid[i][j];
+        if (sum == target) return true;
+    }
+    
+    return false;
+}
