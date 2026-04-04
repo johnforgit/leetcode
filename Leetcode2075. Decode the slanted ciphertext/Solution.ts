44712bb9ad1d628dfdec6d121f3283889095c736
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-04-04 18:10:45 +0530

    Leetcode 2075 in TS

---
+++ Leetcode2075. Decode the slanted ciphertext/Solution.ts
@@ -0,0 +1,18 @@
+const decodeCiphertext = (encodedText, rows) => {
+    if (rows === 1) return encodedText;
+
+    const n = encodedText.length;
+    const cols = Math.floor(n / rows);
+    let res = [];
+
+    for (let c = 0; c < cols; c++) {
+        let r = 0, j = c;
+        while (r < rows && j < cols) {
+            res.push(encodedText[r * cols + j]);
+            r++;
+            j++;
+        }
+    }
+
+    return res.join('').replace(/\s+$/, '');
+};
