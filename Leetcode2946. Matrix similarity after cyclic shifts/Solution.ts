f1102f299e34055eda3f66dec1d0af006f047813
JOHN PHILIP GEORGE <71582782+johnforgit@users.noreply.github.com>
2026-04-05 18:21:29 +0530

    Leetcode 2946 in TS

---
+++ Leetcode2946. Matrix similarity after cyclic shifts/Solution.ts
@@ -0,0 +1,14 @@
+function areSimilar(mat: number[][], k: number): boolean {
+    const m: number = mat.length;
+    const n: number = mat[0].length;
+    k %= n;
+
+    for (let i: number = 0; i < m; i++) {
+        for (let j: number = 0; j < n; j++) {
+            if (mat[i][j] !== mat[i][(j + k) % n]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
