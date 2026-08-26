5a4e4ce92ca6f41093796a9c465363c42e7e62c6
JOHNGEO-5CG5092\johngeo <john.p.george@oracle.com>
2026-08-27 00:23:34 +0530

    modified C++ solution

--- leetcode2904/Solution.cpp
+++ leetcode2904/Solution.cpp
@@ -1,39 +1,42 @@
 using namespace std;
-// sliding window solution
+/**
+ * ### SLIDING WINDOW SOLUTION ###
+ */
 class Solution {
 public:
-    string shortestBeautifulSubstring(string s, int k) {
-        if (ranges::count(s, '1') < k) {
-            return "";
-        }
-        string ans = s;
-        int cnt = 0;
-        for (int left = 0, right = 0; right < s.length(); right++) {
-            cnt += s[right] - '0';
-            while (cnt > k || s[left] == '0') {
-                cnt -= s[left] - '0';
-                left++;
-            }
-            if (cnt == k) {
-                string t = s.substr(left, right - left + 1);
-                if (t.length() < ans.length() ||
-                    t.length() == ans.length() && t < ans) {
-                    ans = move(t);
-                }
-            }
-        }
-        return ans;
-    }
+     string shortestBeautifulSubstring(string s, int k) {
+          if (ranges::count(s, '1') < k) {
+               return "";
+          }
+          string ans = s;
+          int cnt = 0;
+          for (int left = 0, right = 0; right < s.length(); right++) {
+               cnt += s[right] - '0';
+               while (cnt > k || s[left] == '0') {
+                    cnt -= s[left] - '0';
+                    left++;
+               }
+               if (cnt == k) {
+                    string t = s.substr(left, right - left + 1);
+                    // check for smallest length wise and lexicographically
+                    if (t.length() < ans.length() ||
+                         t.length() == ans.length() && t < ans) {
+                         ans = move(t);
+                    }
+               }
+          }
+          return ans;
+     }
 };
 
-// solution using enumeration
 /**
-     class Solution {
-public:
-    string shortestBeautifulSubstring(string s, int k) {
-        for (int m = k; m <= s.length(); m++) {
-            string ans = "";
-            for (int i = m; i <= s.length(); i++) {
+ * ### SOLUTION USING ENUMERATION ###
+ * class Solution {
+ * public:
+ *   string shortestBeautifulSubstring(string s, int k) {
+ *        for (int m = k; m <= s.length(); m++) {
+ *             string ans = "";
+ *             for (int i = m; i <= s.length(); i++) {
                 string t = s.substr(i - m, m);
                 if ((ans.empty() || t < ans) && ranges::count(t, '1') == k) {
                     ans = t;
